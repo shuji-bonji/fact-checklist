88e46c6d1d46da748e150e54aae24b12e5edbad5
bonji <bonji@mikuro.jp>
2025-08-17 05:37:55 +0900

    fix: og-imagesの変更

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
         workbox: {
           // version: '1.0.0'
           globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff,woff2}'],
+          maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3MBまでキャッシュ可能にする
           navigateFallback: null,
           cleanupOutdatedCaches: true,
           clientsClaim: true,
